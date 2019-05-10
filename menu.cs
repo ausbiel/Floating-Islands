e168f4f9b2b6ce9bc1786cd04fa3fe379642deec
ausbiel <39152097+ausbiel@users.noreply.github.com>
2019-05-10 12:47:58 +0300

    Add files via upload
    Menu script

---
+++ menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI﻿;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void OpenCredits()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
